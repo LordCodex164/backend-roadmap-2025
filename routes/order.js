4c1e32fd1cf1de542259b29fa85ca3d1c0784479
lordcodex dev <adenirandaniel565@gmail.com>
2025-05-25 00:48:32 +0100

    FEAT: implemented stripe payment integration

--- routes/order.js
+++ routes/order.js
@@ -7,8 +7,14 @@ const router = express.Router()
 
 router.get("/", createCrsfToken, isAuth, orderController.getOrders)
 
-router.post("/checkout", createCrsfToken, isAuth, orderController.checkoutOrder)
+router.post("/checkout-order", createCrsfToken, isAuth, orderController.checkoutOrder)
 
 router.get("/get_invoice/:order_id", createCrsfToken, isAuth, orderController.downloadInvoice)
+
+router.get("/checkout", isAuth, createCrsfToken, orderController.getCheckout)
+
+router.get("/checkout/success", isAuth, createCrsfToken, orderController.checkoutSuccess)
+
+router.get("/checkout/cancel", isAuth, createCrsfToken, orderController.checkoutOrder)
 
 module.exports = router
